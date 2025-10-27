6428645cf3e519622ce19d9f5c29f8ab10e2b251
vrushitpatel <vrushit7506@gmail.com>
2025-10-28 02:01:44 +0530

    Implemented Wipe Functionality

--- app/routes.ts
+++ app/routes.ts
@@ -5,6 +5,7 @@ export default [
   route("/auth", 'routes/auth.tsx'),
   route("/upload", 'routes/upload.tsx'),
   route("/resume/:id", 'routes/resume.tsx'),
+  route("/wipe", 'routes/wipe.tsx'),
   // Handle Chrome DevTools requests
   route("/.well-known/appspecific/com.chrome.devtools.json", "routes/devtools.tsx"),
 ] satisfies RouteConfig;
